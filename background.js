77b136e641c027235023e933cdb960b85812beaa
T. Devon Artis <devon@devonartis.com>
2024-08-02 20:00:17 -0400

    updated manifest file

--- background.js
+++ background.js
@@ -4,18 +4,35 @@ chrome.runtime.onInstalled.addListener(() => {
     });
   });
   
+  let isShowingWarning = false;
+  
   chrome.tabs.onCreated.addListener((tab) => {
+    if (isShowingWarning) {
+      return; // Ignore tab creation if we're showing the warning
+    }
+  
     chrome.tabs.query({}, (tabs) => {
       chrome.storage.sync.get('maxTabs', (data) => {
         if (tabs.length > data.maxTabs) {
           chrome.tabs.remove(tab.id);
+          
+          isShowingWarning = true;
           chrome.windows.create({
             url: 'warning.html',
             type: 'popup',
             width: 300,
             height: 200
+          }, (window) => {
+            // Listen for the warning window to close
+            chrome.windows.onRemoved.addListener(function listener(windowId) {
+              if (windowId === window.id) {
+                isShowingWarning = false;
+                chrome.windows.onRemoved.removeListener(listener);
+              }
+            });
           });
         }
       });
     });
   });
+  
